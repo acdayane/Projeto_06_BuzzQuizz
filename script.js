adaa271f22df5c8a12451f32aa02a700dabfb72b
Dayane <dayaneac@hotmail.com>
2022-09-01 11:40:34 -0300

    telas 1 e 2 - botao vermelho e get de um quizz

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let quizzes;
+let idQuizz;
 
 
 // Obter quizzes que não são do usuário para tela 1
@@ -11,14 +13,14 @@ function obterQuizzes () {
 
 function renderizarQuizzes (res) {
     console.log('Deu certo o recebimento dos quizzes');
-    const quizzes = res.data;
+    quizzes = res.data;
     console.log(res.data);
 
     const listaQuizzes = document.querySelector('.lista-quizzes');
 
     for (let i=0; i<quizzes.length; i++){
         listaQuizzes.innerHTML += `
-        <li class = 'quizz'>
+        <li class = 'quizz' onclick = 'buscarQuizz(${quizzes[i].id})'>
             <span>${quizzes[i].title}</span>
             <img src = '${quizzes[i].image}' />
         </li>
@@ -31,7 +33,47 @@ function deuErro(err) {
     alert("Algo deu errado. Por favor, recarregue a página.");  
 } 
 
+
+// Abrir quizz selecionado na tela 2
+function buscarQuizz(idQuizz) {
+    
+    promessa = axios.get (`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${idQuizz}`);
+    promessa.then(abrirQuizz, console.log(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${idQuizz}`));
+    promessa.catch(deuErro);
+
+}
+
+function abrirQuizz(res) {
+
+    let quizzClicado = res.data; 
+    console.log(quizzClicado)
+
+removeTela();
+
+    function removeTela(){
+        let removeTela1 = document.querySelector ('.conteudo-principal')
+        removeTela1.classList.add ('none');    
+    }
+
+    const umQuizz = document.querySelector('.um-quizz');
+    umQuizz.classList.remove ('none');
+       
+    umQuizz.innerHTML = `
+        <div class = 'quizz'>
+            <span>${quizzClicado.title}</span>
+            <img src = '${quizzClicado.image}' />
+        </div>
+        ` 
+    console.log(quizzClicado.title)
+
+    console.log (umQuizz);
+}       
+
+
+
 function criaQuizz(){
     let criandoQuizz= document.querySelector(".criando-quizz");
     criandoQuizz.classList.remove("none");
 }
+
+
